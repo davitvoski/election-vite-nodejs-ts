167f491dbfbb515b8a3ace457dfbfb4867157cef
davitvoski <davitvoskerchyan@outlook.com>
2022-12-17 16:14:46 -0500

    Add Redis caching connection

--- server/app.ts
+++ server/app.ts
@@ -1,8 +1,24 @@
+import dotenv from "dotenv"
+dotenv.config()
 import * as express from "express"
 import compression from "compression"
+import * as redis from "redis"
 import quebecElectionRouter from "./routes/quebecElection.router"
 
+const redisClient = redis.createClient({ 
+    url: "redis://default:fRMAbrfGsCIQx1GGjMRfBYpZiQcfADoQ@redis-14640.c10.us-east-1-3.ec2.cloud.redislabs.com:14640"
+});
+(async () => {
+    try {
+        await redisClient.connect()
+    } catch (err) {
+        console.log(`Redis-Error: ${err}`)
+        process.exit(1)
+    }
+})();
+
 const app = express.default()
+
 app.use(compression())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
@@ -17,3 +33,4 @@ app.use((_, res) => {
 });
 
 export default app
+export { redisClient }
