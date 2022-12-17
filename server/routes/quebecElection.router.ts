f019bab25ae298d1381b328b512a34a2bc54a3c0
davitvoski <davitvoskerchyan@outlook.com>
2022-12-17 15:24:33 -0500

    Forgot to commit routes : 2 commits before

---
+++ server/routes/quebecElection.router.ts
@@ -0,0 +1,10 @@
+import * as express from "express"
+import { getCirconscriptionVoteDetails, getTopoJsonData } from "../controller/quebec/quebecElection.controller"
+const router = express.Router()
+
+
+router.get("/2022/circonscription/:numeroCirco", getCirconscriptionVoteDetails)
+router.get("/map", getTopoJsonData)
+
+
+export default router
