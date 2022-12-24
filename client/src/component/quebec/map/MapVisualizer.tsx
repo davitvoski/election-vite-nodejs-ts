6fef2d486869564a24cc64bc2662b814a71532dd
davitvoski <davitvoskerchyan@outlook.com>
2022-12-23 20:42:00 -0500

    Cleaned

--- client/src/component/quebec/map/MapVisualizer.tsx
+++ client/src/component/quebec/map/MapVisualizer.tsx
@@ -8,12 +8,11 @@ import MapTable from "./MapTable"
 export default function MapVisualizer() {
     const [content, setContent] = useState<string>("Hover over district")
     const [dataTable, setDataTable] = useState<IGeometry>()
-    
+
     return (
         <section className="mt-5 container flex flex-row w-full flex-grow text-center justify-center items-center h-1/2">
-            <div className="m-6 xl:w-8/12 ck flex-block items-center justify-center">
+            <div className="ml-20 xl:w-8/12 ck flex-block items-center justify-center">
                 <div className='border-2 border-black text-center'>
-                    {/* <div className='border-2 border-black text-center' style={{ backgroundColor: "#f5f4f6" }}> */}
                     <h1 className="ml-5 inline-block text-black text-3xl"> Quebecs' Electoral Division Wins </h1>
                     <MapChart setContentToolTip={setContent} setTable={setDataTable} />
                     <Tooltip anchorId="regionName-tip">{content}</Tooltip>
@@ -26,3 +25,4 @@ export default function MapVisualizer() {
         </section>
     )
 }
+
