5b3e506e6dc7ef4b1a980de6fbeddf10dfa9fea6
davitvoski <davitvoskerchyan@outlook.com>
2023-01-02 13:47:46 -0500

    Add Routes

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,46 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App'
+import { BC } from './component/canada/british-colombia/BC'
+import QC from './component/canada/quebec/QC'
 import './index.css'
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children:
+      [
+        {
+          index: true,
+          element: <QC />
+        },
+        {
+
+          path: "/quebec/:year",
+          element: <QC />
+        },
+        {
+          path: "/british-columbia/:year",
+          element: <BC />,
+
+
+        },
+        {
+          path: "*",
+          element: <h1>404 not found</h1>
+        }
+      ]
+  }
+]
+)
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
