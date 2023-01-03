87e7001bae5a37012466a744db54d86d16d41ef8
davitvoski <davitvoskerchyan@outlook.com>
2023-01-02 22:47:22 -0500

    Add PArties CArd

--- client/src/component/canada/british-colombia/BC.tsx
+++ client/src/component/canada/british-colombia/BC.tsx
@@ -1,19 +1,25 @@
+import { useState } from "react"
 import { useParams } from "react-router-dom"
+import { IBCParty } from "../../../types/Parties"
 import MapVisualizerBC from "./map/MapVisualizerBC"
+import PartiesCardsVisualizerBC from "./parties/PartiesCardsVisualizerBC"
 
 export function BC() {
-    let { year } = useParams()
-    year = "2020"
+    const year = "2020"
+    const [politicalParties, setPoliticalParties] = useState<IBCParty[]>();
 
     return (
         <>
             <main className='flex flex-col container'>
+                <h2 className={`text-4xl text-center mt-4`}>Election Parties</h2>
+                <PartiesCardsVisualizerBC politicalParties={politicalParties!}
+                    setPoliticalParties={setPoliticalParties} year={year!} />
 
                 <h2 className={`text-4xl mt-4 text-center`}>Election District Results</h2>
                 <section className='flex flex-col items-center'>
                     <MapVisualizerBC year={year!} />
-                    {/* <CirconscriptionVisualizer year={year!} /> */}
                 </section>
+
             </main>
         </>
     )
--- client/src/component/canada/british-colombia/map/MapVisualizerBC.tsx
+++ client/src/component/canada/british-colombia/map/MapVisualizerBC.tsx
@@ -2,7 +2,6 @@ import { useState } from "react"
 import { Tooltip } from "react-tooltip"
 import { Geometry } from "../../../../types/british-colombia/IMapBC"
 import MapChartBC from "./MapChartBC"
-import MapTableBC from "./MapTableBC"
 
 
 export default function MapVisualizerBC({ year }: { year: string }) {
@@ -10,17 +9,13 @@ export default function MapVisualizerBC({ year }: { year: string }) {
     const [dataTable, setDataTable] = useState<Geometry>()
 
     return (
-        <section className="mt-5 container flex flex-row w-full flex-grow text-center justify-center items-center h-1/2">
-            <div className="ml-20 xl:w-8/12 ck flex-block items-center justify-center">
+        <section className="mt-5 container flex flex-row w-full flex-grow text-center justify-center items-center">
+            <div className="ml-20 mb-4 w-7/12 flex-block items-center justify-center">
                 <div className='border-2 border-black text-center'>
                     <h1 className="ml-5 inline-block text-black text-3xl"> Quebecs' Electoral Division Wins </h1>
                     <MapChartBC setContentToolTip={setContent} setTable={setDataTable} year={year} />
                     <Tooltip anchorId="regionName-tip">{content}</Tooltip>
                 </div>
-            </div>
-            <div className="xl:w-10/12 flex flex-grow">
-                {dataTable ? <MapTableBC year={year} geo={dataTable} /> :
-                    <h1 className="w-full self-center text-center text-3xl">Click District To Load Table</h1>}
             </div>
         </section>
     )
