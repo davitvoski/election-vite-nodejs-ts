567cf158499bb31f6ad4dda38dd73e7f92d7d3d4
davitvoski <davitvoskerchyan@outlook.com>
2023-01-04 15:42:55 -0500

    Random

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,6 @@
 import { IParty } from './types/QuebecTypes'
-import { SelectHTMLAttributes, SyntheticEvent } from 'react'
-import { Link, Outlet, Route, Routes, useNavigate, useParams } from 'react-router-dom'
-
-
-export type IPartyVisualizer = IParty & {
-  id: number,
-  color: string
-}
+import { createContext, SelectHTMLAttributes, SyntheticEvent, useContext, useRef } from 'react'
+import { Link, Outlet, Route, Routes, useNavigate, useOutletContext, useParams } from 'react-router-dom'
 
 
 function App() {
@@ -34,7 +28,7 @@ function App() {
   return (
     <>
       <header className='flex flex-col justify-center'>
-        <h1 className='text-4xl text-center'>Candian Elections</h1>
+        <h1 className='text-4xl text-center'>Canadian Elections</h1>
         <nav className='flex justify-center gap-10' onChange={changeYearHandler}>
           <span>
             <label>State:</label>
@@ -50,7 +44,7 @@ function App() {
 
           <span>
             <label>Year:</label>
-            <select id="election-year" name="election-year" value={year} >
+            <select id="election-year" name="election-year" defaultValue={year}>
               <option value="2022">2022</option>
               <option value="2018">2018</option>
 
@@ -60,38 +54,7 @@ function App() {
         </nav>
       </header>
 
-      {/* <Switch>
-        <Route path="/quebec/:year" />
-        <Route path="/british-columbia" />
-      </Switch> */}
       <Outlet />
-
-      {/* <main className='flex flex-col container'> */}
-      {/* <h2 className={`text-4xl text-center mt-4`}>Election Parties</h2>
-        <PartiesCardsVisualizer politicalParties={politicalParties!}
-          setPoliticalParties={setPoliticalParties} year={year} />
-
-        <section className='flex gap-10 justify-center'>
-          {politicalParties &&
-            <ValidBallots parties={politicalParties} />
-          }
-          {politicalParties &&
-            <ValidBallotsPercentage parties={politicalParties} />
-          }
-          {politicalParties &&
-            <ElectedCandidates parties={politicalParties} />
-          }
-
-        </section>
-
-        <h2 className={`text-4xl mt-4 text-center`}>Election District Results</h2>
-        <section className='flex flex-col items-center'>
-          <MapVisualizer year={year} />
-          <CirconscriptionVisualizer year={year} />
-        </section> */}
-
-      {/* </main> */}
-
     </>
   )
 }
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { createContext, useMemo, useState } from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App'
@@ -37,10 +37,9 @@ const router = createBrowserRouter([
 ]
 )
 
-
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+
   <React.StrictMode>
-    {/* <App /> */}
     <RouterProvider router={router} />
   </React.StrictMode>,
 )
